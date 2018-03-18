50d0934f66d3f7fa08aff71791fae451c436ba88
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-03-18 15:12:25 +0100

    Add a color preview (enabled by default), add basic input validation
    The preview can be disabled by passing `--no-preview` to the command
    line.

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const chalk = require('chalk');
  * Convert a RGB or RGBA color to an hexadecimal code (#RRGGBB or #RRGGBBAA).
  *
  * @param color The Color object to convert.
+ * @return string
  */
 function rgbToHex(color) {
   const red = Math.round(color.red()).toString(16).padStart(2, '0');
@@ -49,12 +50,24 @@ function rgbToHex(color) {
 }
 
 /**
+ * Convert a color to a `bgRGB(r, g, b)` color string, used for coloring
+ * the output using `chalk`.
+ *
+ * @param color The Color object to convert.
+ * @return string
+ */
+function rgbToBgRgb(color) {
+  return `bgRgb(${Math.round(color.red())}, ${Math.round(color.green())}, ${Math.round(color.blue())})`;
+}
+
+/**
  * Displays a color in various formats.
  *
- * @param logger The logger object from Caporal.
- * @param color The Color object to display.
+ * @param {Logger} logger The logger object from Caporal.
+ * @param {Color} color The Color object to display.
+ * @param {boolean} noPreview Whether to hide the preview of the color.
  */
-function displayColor(logger, color) {
+function displayColor(logger, color, noPreview) {
   logger.info();
   logger.info(chalk`  {white.bold HEX}   ${rgbToHex(color)}`);
   logger.info(chalk`  {white.bold RGB}   ${color.rgb().string()}`);
@@ -63,6 +76,18 @@ function displayColor(logger, color) {
   // messing with the displayed value
   logger.info(chalk`  {white.bold HSV}   ${color.hsl().round().string()}`);
   logger.info();
+
+  // Display a color preview on a black and white background (if enabled)
+  if (!noPreview) {
+    logger.info(chalk`          {bgWhite           }`);
+
+    for (let i = 0; i < 2; i += 1) {
+      logger.info(chalk`   {${rgbToBgRgb(color)}     }   {bgWhite    {${rgbToBgRgb(color)}     }   }`);
+    }
+
+    logger.info(chalk`          {bgWhite           }`);
+    logger.info();
+  }
 }
 
 // Create the command line interface
@@ -71,56 +96,64 @@ program
 
   .command('info', 'Display a color in various formats')
   .argument('<color>', 'The color string to display information about')
+  .option('--no-preview', 'Hide the color preview', program.BOOLEAN)
   .action((args, options, logger) => {
     const color = Color(args.color);
-    displayColor(logger, color);
+    displayColor(logger, color, options.noPreview);
   })
 
   .command('negate', 'Invert and display a color')
   .argument('<color>', 'The color string to invert and display')
   .action((args, options, logger) => {
     const color = Color(args.color).negate();
-    displayColor(logger, color);
+    displayColor(logger, color, options.noPreview);
   })
 
   .command('lighten', 'Lighten and display a color')
   .argument('<color>', 'The color string to lighten and display')
-  .argument('<amount>', 'The amount to lighten the color by (0.0..1.0)')
+  .argument('<amount>', 'The amount to lighten the color by (0.0..1.0)', program.FLOAT)
   .action((args, options, logger) => {
     const color = Color(args.color).lighten(args.amount);
-    displayColor(logger, color);
+    displayColor(logger, color, options.noPreview);
   })
 
   .command('darken', 'Darken and display a color')
   .argument('<color>', 'The color string to darken and display')
-  .argument('<amount>', 'The amount to darken the color by (0.0..1.0)')
+  .argument('<amount>', 'The amount to darken the color by (0.0..1.0)', program.FLOAT)
   .action((args, options, logger) => {
     const color = Color(args.color).darken(args.amount);
-    displayColor(logger, color);
+    displayColor(logger, color, options.noPreview);
   })
 
   .command('saturate', 'Saturate and display a color')
   .argument('<color>', 'The color string to saturate and display')
-  .argument('<amount>', 'The amount to saturate the color by (0.0..1.0)')
+  .argument('<amount>', 'The amount to saturate the color by (0.0..1.0)', program.FLOAT)
   .action((args, options, logger) => {
     const color = Color(args.color).saturate(args.amount);
-    displayColor(logger, color);
+    displayColor(logger, color, options.noPreview);
   })
 
   .command('desaturate', 'Desaturate and display a color')
   .argument('<color>', 'The color string to desaturate and display')
-  .argument('<amount>', 'The amount to desaturate the color by (0.0..1.0)')
+  .argument('<amount>', 'The amount to desaturate the color by (0.0..1.0)', program.FLOAT)
   .action((args, options, logger) => {
     const color = Color(args.color).desaturate(args.amount);
-    displayColor(logger, color);
+    displayColor(logger, color, options.noPreview);
   })
 
   .command('rotate', 'Rotate the hue and display a color')
   .argument('<color>', 'The color string to hue-rotate and display')
-  .argument('<degrees>', 'The number of degrees to rotate the color\'s hue by')
+  .argument('<degrees>', 'The number of degrees to rotate the color\'s hue by', program.FLOAT)
   .action((args, options, logger) => {
     const color = Color(args.color).rotate(args.degrees);
-    displayColor(logger, color);
+    displayColor(logger, color, options.noPreview);
   });
+
+// Add the options from the `info` command to all commands
+const defaultCmd = program._commands[0];
+
+for (const cmd of program._commands.slice(1)) {
+  cmd._options.unshift(...defaultCmd._options);
+}
 
 program.parse(process.argv);
